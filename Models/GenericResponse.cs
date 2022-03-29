0322c19e16bee2249a81e47038a79e1da7ebe757
Ahmed Alnjjar <aalnjjar32@gmail.com>
2022-03-29 14:13:52 +0300

    helpers

---
+++ Models/GenericResponse.cs
@@ -0,0 +1,22 @@
+﻿namespace ANUPayments.Models
+{
+    public class GenericResponse<T, TU>
+    {
+        public GenericResponse()
+        {
+        }
+
+        public GenericResponse(string jsonResponse, T successResponse, TU failureResponse)
+        {
+            IsSuccess = failureResponse == null;
+            JsonResponse = jsonResponse;
+            SuccessResponse = successResponse;
+            FailureResponse = failureResponse;
+        }
+
+        public bool IsSuccess { get; set; }
+        public string JsonResponse { get; set; }
+        public T SuccessResponse { get; set; }
+        public TU FailureResponse { get; set; }
+    }
+}
